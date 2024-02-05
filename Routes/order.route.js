2bbce8dc9c5311650266fdb5e15ddb180e8ae93c
iTsmeSAADi <saadshah269@gmail.com>
2024-02-05 20:40:17 +0500

    updated create order

--- Routes/order.route.js
+++ Routes/order.route.js
@@ -8,13 +8,13 @@ const upload = multer();
 
 
 router.post("/orders", auth.verifyUserCookie, auth.verifyApproved, auth.verifyNotDriver, order.stationOrders);
-router.post("/create", order.createOrder);
+router.post("/create", upload.single("attachment"), auth.verifyUserCookie,auth.verifyApproved, auth.verifyCompanyId, auth.verifyOrderManager, order.createOrder);
 router.post("/company", auth.verifyUserCookie, auth.verifyApproved, auth.verifyAdmin, order.allCompanyQueryOrders)
 router.post("/search", auth.verifyUserCookie, auth.verifyApproved, auth.verifyNotDriver, order.queryOrder)
 router.patch("/update", auth.verifyUserCookie, auth.verifyApproved, order.updateOrder)
 router.get("/:id", auth.verifyUserCookie, auth.verifyApproved, auth.verifyNotDriver, order.getOrder)
 router.post("/report", auth.verifyUserCookie, auth.verifyApproved, order.generateAndMailOrdersReport);
-router.post("/driver/assign", order.driverAssignOrder)
+router.post("/driver/assign", auth.verifyUserCookie, auth.verifyApproved, auth.verifyDriver, order.driverAssignOrder)
 router.post("/driver/recieved", upload.single("attachment"), auth.verifyUserCookie, auth.verifyApproved, auth.verifyDriver, order.driverRecievedOrder)
 router.post("/driver/delievered", auth.verifyUserCookie, auth.verifyApproved, auth.verifyDriver, order.driverDelieveredOrder);
 router.post("/manager/complete", upload.single("attachment"), auth.verifyUserCookie, auth.verifyApproved, auth.verifySeniorStationManager, order.ManagerCompleteOrder);
