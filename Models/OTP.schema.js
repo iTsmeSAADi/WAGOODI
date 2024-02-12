ff657fd7273597773a35e13d316c771a913a3658
iTsmeSAADi <saadshah269@gmail.com>
2024-02-12 15:37:11 +0500

    time date

--- Models/OTP.schema.js
+++ Models/OTP.schema.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const OTPSchema = new mongoose.Schema({
     accountId: {type: mongoose.Types.ObjectId, ref:"acoount", required: true},
     otp: {type: "String", required: true},
-    expiresAt: {type: Number, default: (Math.floor(Date.now() / 1000)  + 300)},
+    expiresAt: {
+        type: Number,
+        default: () => Math.floor(Date.now() / 1000) + 30,  // 30 seconds from the current time
+      },      
 }, {versionKey: false})
 
 const OTP = mongoose.model('otp', OTPSchema);
