9c964d8c3eb2228cd49782ee4aaedb5d0bd77b31
iTsmeSAADi <saadshah269@gmail.com>
2024-02-14 18:28:58 +0500

    io changes

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ const PORT = process.env.PORT;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: true } });
 
 app.io = io;
 app.set("io", io)
